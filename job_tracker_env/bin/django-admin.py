67015d311252cfa559280398145e2e8374f61f46
Andrew Rosario <rosario.andrew@gmail.com>
2019-12-04 20:59:35 -0600

    update django version to 3.0

--- job_tracker_env/bin/django-admin.py
+++ job_tracker_env/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/Users/andrewrosario/Development/code/Python/job_tracker/job_tracker_backend/job_tracker_env/bin/python3
+#!/Users/andrewrosario/Development/code/Python/job_tracker/job_tracker_backend/job_tracker_env/bin/python
 from django.core import management
 
 if __name__ == "__main__":
